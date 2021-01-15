b3d4a817108030734e13147e721e9565b921103c
Vahid Khodabakhshi <khodabakhshi.vahid@gmail.com>
2021-01-15 22:46:22 +0330

    some minor improvements

--- src/json-analysis.py
+++ src/json-analysis.py
@@ -15,6 +15,9 @@ import glob, os
 import json
 from time import sleep
 
+from anytree import Node, RenderTree
+from anytree.exporter import DotExporter
+
 '''----------------------------------
 The function read all JSON files and
 store data in results data structure
@@ -38,37 +41,48 @@ fills out results data structure.
 ----------------------------------'''
 def analysis( data, results ):
     
-    # filling out the number of each states (found, looped, exceed-threshold)
     state = data['crawl-final-state']
+    
     if state.find( 'Unfortunately' )!=-1:
+        # fill out `exceed-threshold`
         results['exceed-threshold'] += 1
+        
     elif state.find( 'loop' )!=-1:
+        # fill out `looped`
         results['looped'] += 1
+        # fill out `looped-links`
         looped_link = list( data['search-history'].keys() )[-1] # get the last element of the list which is repeated!
         results['looped-links'].append( looped_link )
+        
     else:
+        # fill out `found`
         results['found'] += 1
+        # fill out `distance-to-ph`
+        i = int( state.split('after')[1].split(' ')[1] )
+        results['distance-to-ph'][ i ] = results['distance-to-ph'].get( i, 0 ) + 1
     
+    # fill out `search-history`
     results['search-history'] += list( data['search-history'].keys() ) # get all search history including loops, unsuccessfull and successfull crawls!
+    
+    # fill out `sorted_titles`
+    links_count = dict()
+    for i in results['search-history']:
+        links_count[ i ] = links_count.get( i, 0 ) + 1
+    results['sorted_titles'] = { k: v for k, v in sorted( links_count.items(), key=lambda item: -item[1] ) }
 
 '''--------------
 Main function.
 --------------'''
 def main():
     
-    results = { 'found':0, 'looped':0, 'exceed-threshold':0, 'looped-links':[], 'search-history':[], 'distance-to-ph':{} }
+    results = { 'found':0, 'looped':0, 'exceed-threshold':0, 'looped-links':[], 'sorted_titles':[], 'search-history':[], 'distance-to-ph':{} }
     
     read_files( '.\\json\\', results )
     
     results['looped-links'] = set( results['looped-links'] )
-    # results['search-history'] = set( results['search-history'] )
-    
-    links_count = dict()
-    for i in results['search-history']:
-        links_count[ i ] = links_count.get( i, 0 ) + 1
-    results['search-history'] = { k: v for k, v in sorted( links_count.items(), key=lambda item: -item[1] ) }
     
     print( results )
+    # make_tree(  )
     
 if __name__ == "__main__":
     main()
